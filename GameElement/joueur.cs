4e7fa25e00201702b3229b1780781309dd3363f5
Flink88 <meanf.isat+github@gmail.com>
2015-08-17 14:11:31 +0200

    ajout joueur.cs

---
+++ GameElement/joueur.cs
@@ -0,0 +1,12 @@
+﻿namespace GameElement
+{
+    class joueur
+    {
+        public string Pseudo { get; set; }
+
+        public joueur(string pseudo)
+        {
+            this.Pseudo = pseudo;
+        }
+    }
+}
